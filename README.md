0dcdb4de92e080572fafc0be28640c0eb455c3e8
Minwook Yi <grs0416@gmail.com>
2025-06-14 17:03:00 +0900

    #DEV#NEW - mac 환경 설정

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# wasm
+# Version
 
-emscripten --version 3.1.40
-
+emscripten 3.1.40
 python 3.12.5
+cmake 4.0.2
+
+# WASM 설치
 
 ```
-emcc -lembind -o <outfile .js> <source .cpp>
+git clone https://github.com/emscripten-core/emsdk.git
+git pull
+./emsdk install latest
+./emsdk activate latest
+```
 
+# cmake for mac
+
+```
+brew install cmake
+```
+
+# WASM 명령어
+
+```
+source ./emsdk_env.sh # 터미널을 새로 열때마다 환경설정
+emcc -lembind -o <outfile .js> <source .cpp>
 ```
--- example/call_jsfunc/call_jsfunc.cpp
+++ example/call_jsfunc/call_jsfunc.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-EM_JS(void, call_js, (const string subject, const char *msg), {
-  jsFunction(UTF8ToString(subject), UTF8ToString(msg));
+// UTF8ToString (포인터, 길이) - emscripten 내장 유틸함수
+// https://emscripten.org/docs/api_reference/preamble.js.html?utm_source=chatgpt.com#UTF8ToString
+EM_JS(void, call_js, (const char *subject, int subject_len, const char *msg, int msg_len), {
+  jsFunction(UTF8ToString(subject, subject_len), UTF8ToString(msg, msg_len));
 });
 
 bool my_calls()
 {
   const string subject = "제목";
   const string msg = "메세지";
-  call_js(subject, msg.c_str());
+  call_js(subject.c_str(), subject.length(), msg.c_str(), msg.length());
   return true;
 }
 
